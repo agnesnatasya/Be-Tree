ecc02eee900eec0f8d757d22309205dc3c5e6ff6
agnesnatasya <agnesnatasyawijaya@gmail.com>
2021-10-20 23:32:36 +0800

    feat: change obj included in network compilation

--- network/Rules.mk
+++ network/Rules.mk
@@ -3,6 +3,7 @@ d := $(dir $(lastword $(MAKEFILE_LIST)))
 FILES=transport.cpp configuration.cpp
 ifeq ($(IS_DEV),0)
 FILES += fasttransport.cpp
+echo halo
 endif
 
 SRCS += $(addprefix $(d), $(FILES))
@@ -11,4 +12,8 @@ LIB-configuration := $(o)configuration.o $(LIB-debug)
 
 LIB-transport := $(o)transport.o $(LIB-configuration) $(LIB-debug)
 
-LIB-fasttransport := $(o)fasttransport.o $(LIB-transport) $(LIB-debug)
+LIB-fasttransport := $(LIB-transport) $(LIB-debug)
+
+ifeq ($(IS_DEV),0)
+LIB-fasttransport += $(o)fasttransport.o
+endif
