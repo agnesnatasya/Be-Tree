ef30bb97673f685fb0a4d71a3e1dd0ab187e197a
agnesnatasya <agnesnatasyawijaya@gmail.com>
2021-10-21 23:13:58 +0800

    fix: callback server client

--- network/simtransport.cpp
+++ network/simtransport.cpp
@@ -115,7 +115,7 @@ void SimTransport::Run()
     // If it's server, check the request queue
     while(!stop) {
         if (!c->rpc->req_queue.empty()) {
-            cout << "There's a message";
+            std::cout << "There's a message";
             sim_req_tag_t* req_tag = c->rpc->req_queue.front();
             req_tag->src->ReceiveRequest(req_tag->reqType, req_tag->req_msgbuf, req_tag->resp_msgbuf);
         }
@@ -131,7 +131,7 @@ bool SimTransport::SendRequestToServer(TransportReceiver *src, uint8_t reqType, 
     c->client.crt_req_tag->src = src;
     c->client.crt_req_tag->reqType = reqType;
     c->rpc->enqueue_request(c->client.crt_req_tag);
-    cout << "Request enqueued!";
+    std::cout << "Request enqueued!";
     return true;
 }
 
