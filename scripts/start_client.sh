af96663da79de766eb355f454f272b58f8f487c0
Adriana Szekeres <aszekeres@vmware.com>
2021-11-18 22:37:39 -0800

    script to start client

---
+++ scripts/start_client.sh
@@ -0,0 +1,3 @@
+BTREE_PATH='/home/ubuntu/aszekeres/Be-Tree'
+
+$BTREE_PATH/client/client --backingStoreDir $BTREE_PATH/store --benchmark benchmark-upserts --configFile $BTREE_PATH/config.txt --clientIP "10.172.209.107"
